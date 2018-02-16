05c98d5c31750fa6343f73fc330c2e3d8761f0d3
abdulmuqeethmohammed <amoham62@uic.edu>
2018-02-15 22:06:21 -0600

    Adding menu to the TaskDetailsActivity

--- app/src/main/java/com/abdulmuqeethmohammed/todoapp/TaskDetailsActivity.java
+++ app/src/main/java/com/abdulmuqeethmohammed/todoapp/TaskDetailsActivity.java
@@ -8,6 +8,9 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -27,6 +30,17 @@ public class TaskDetailsActivity extends AppCompatActivity {
         submitTaskButton = (FloatingActionButton) findViewById(R.id.submitTaskButton);
 
         submitTaskButton.setOnClickListener(submitButton);
+    }
+
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        //TODO respond to menu item selecetion
+        return true;
     }
 
     //Capturing the text and returning to calling activity when done button is pressed
